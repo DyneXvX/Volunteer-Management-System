7b88fbb7608286aeb18310527dc5f80a6afb8c30
Justin Thoms <DyneXvX@outlook.com>
2020-07-14 11:27:25 -0400

    Hard-Coded User Name and Password Setup Index Layout to reflect lack of options without login. Setup Admin View Page for Lital to work with

---
+++ VMS.Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VMS.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
--- VMS/Areas/Admin/Controllers/HomeController.cs
+++ VMS/Areas/Admin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VMS.Models.ViewModels;
 
+
 namespace VMS.Areas.Admin.Controllers
 {
     [Area("Admin")] //Only Admin functions in this application.
@@ -30,5 +31,26 @@ namespace VMS.Areas.Admin.Controllers
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
+
+        //this is hard-coded because we do not have a shared database to build on. 
+        //Don't change this or you are not getting in - Would never do this in the real world
+        [HttpPost]
+        public IActionResult Index(LoginViewModel login)
+        {
+            var userName = login.User;
+            var password = login.Password;
+            
+            if (( userName == "admin") && (password ==  "password"))
+            {
+                return View("Admin");
+            }
+            else
+            {
+                
+                return View();
+            }
+        }
+
+
     }
 }
