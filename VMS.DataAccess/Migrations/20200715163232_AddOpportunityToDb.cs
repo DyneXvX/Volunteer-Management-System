0d78703e412f402079b50fdbccdac6554f3e2529
Justin Thoms <DyneXvX@outlook.com>
2020-07-15 12:34:12 -0400

    Opportunity Db is done - Migration finished

---
+++ VMS.DataAccess/Migrations/20200715163232_AddOpportunityToDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VMS.DataAccess.Migrations
+{
+    public partial class AddOpportunityToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Opportunities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OpportunityName = table.Column<string>(nullable: false),
+                    DatePosted = table.Column<DateTime>(nullable: false),
+                    VolunteerPrefersCenter = table.Column<string>(nullable: true),
+                    IsOpen = table.Column<bool>(nullable: false),
+                    VolunteerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Opportunities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Opportunities_Volunteers_VolunteerId",
+                        column: x => x.VolunteerId,
+                        principalTable: "Volunteers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Opportunities_VolunteerId",
+                table: "Opportunities",
+                column: "VolunteerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Opportunities");
+        }
+    }
+}
