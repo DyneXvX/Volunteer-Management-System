178057a817f9e28005ebc69496c4161392d6b026
lital400 <lital400@gmail.com>
2020-07-17 16:51:17 -0400

    Added Admin Authorization and user

--- VMS.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ VMS.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -7,6 +7,8 @@ namespace VMS.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         IVolunteerRepository Volunteer { get; }
+        IOpportunityRepository Opportunity { get; }
+        IApplicationUserRepository ApplicationUser { get; }
         ISP_Call SP_Call { get; }
 
         void Save();
