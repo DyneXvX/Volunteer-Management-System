ceafe9fef40aa8eb9f5022740b08933c817c6fa4
Justin Thoms <DyneXvX@outlook.com>
2020-07-31 10:10:00 -0400

    update to requuired for VolunteerPrefersCenter

---
+++ VMS.DataAccess/Migrations/20200731140910_updateToRequiredVolunteerPrefersCenter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VMS.DataAccess.Migrations
+{
+    public partial class updateToRequiredVolunteerPrefersCenter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VolunteerPrefersCenter",
+                table: "Volunteers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VolunteerPrefersCenter",
+                table: "Volunteers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- VMS.Models/Volunteer.cs
+++ VMS.Models/Volunteer.cs
@@ -1,67 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace VMS.Models
 {
     public class Volunteer
     {
-        [Key]
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
 
 
-        [Required]
-        [MaxLength(25)]
-        public string FirstName { get; set; }
-        
-        [Required]
-        [MaxLength(25)]
-        public string LastName { get; set; }
+        [Required] [MaxLength(25)] public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(25)]
-        public string UserName { get; set; }
+        [Required] [MaxLength(25)] public string LastName { get; set; }
+
+        [Required] [MaxLength(25)] public string UserName { get; set; }
 
         [Required]
         [PasswordPropertyText]
         [MaxLength(25)]
         public string Password { get; set; }
 
-        public string VolunteerPrefersCenter { get; set; }
+        [Required] public string VolunteerPrefersCenter { get; set; }
         public string Skills { get; set; }
         public string Availability { get; set; }
 
-        [Required]
-        public string Address { get; set; }
+        [Required] public string Address { get; set; }
         public string HomePhone { get; set; }
         public string WorkPhone { get; set; }
 
-        [Required]
-        public string CellPhone { get; set; }
+        [Required] public string CellPhone { get; set; }
 
-        [EmailAddress]
-        public string Email { get; set; }
+        [EmailAddress] public string Email { get; set; }
         public string Education { get; set; }
         public string License { get; set; }
 
-        [Required]
-        public string EmergencyContactName { get; set; }
+        [Required] public string EmergencyContactName { get; set; }
         public string EmergencyContactHomePhone { get; set; }
 
-        [Required]
-        public string EmergencyContactCellPhone{ get; set; }
-        
-        [EmailAddress]
-        public string EmergencyContactEmail { get; set; }
+        [Required] public string EmergencyContactCellPhone { get; set; }
+
+        [EmailAddress] public string EmergencyContactEmail { get; set; }
         public bool IsDriversLicenseOnFile { get; set; }
         public bool IsSsCardOnFile { get; set; }
 
-        [Required]
-        public bool IsActive { get; set; }
+        [Required] public bool IsActive { get; set; }
 
-        [Required]
-        public string ApprovalStatus { get; set; }
+        [Required] public string ApprovalStatus { get; set; }
     }
 }
