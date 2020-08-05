f3bb3f2c5a57a453717d61b37a2bc75c8414ae38
Justin Thoms <DyneXvX@outlook.com>
2020-08-05 13:08:50 -0400

    Cleaned Removed using statements Inverted if's wehre possilbe Rewrote Username = Input.Name instead of Email for future displays in the layout page <-- This will only work on new users now. One in db will still display email instead. Admin@gmail.com for example

--- VMS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VMS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using VMS.Models;
 using VMS.Utility;
@@ -24,25 +19,21 @@ namespace VMS.Areas.Identity.Pages.Account
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        //private readonly IEmailSender _emailSender;       // if you want to configure sending emails, uncomment these and implement email class
         private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            //IEmailSender emailSender,
             RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
-            //_emailSender = emailSender;
             _roleManager = roleManager;
         }
 
-        [BindProperty]
-        public InputModel Input { get; set; }
+        [BindProperty] public InputModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -56,7 +47,8 @@ namespace VMS.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+                MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -66,8 +58,7 @@ namespace VMS.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
-            [Required]
-            public string Name { get; set; }
+            [Required] public string Name { get; set; }
             public string Role { get; set; }
         }
 
@@ -81,61 +72,35 @@ namespace VMS.Areas.Identity.Pages.Account
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return Page();
+            var user = new ApplicationUser
             {
-                var user = new ApplicationUser
-                {
-                    UserName = Input.Email,
-                    Email = Input.Email,
-                    Name = Input.Name,
-                    Role = Input.Role
-                };
-                var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
+                UserName = Input.Name,
+                Email = Input.Email,
+                Name = Input.Name,
+                Role = Input.Role
+            };
+            var result = await _userManager.CreateAsync(user, Input.Password);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User created a new account with password.");
 
-                    if (user.Role == null)     // role is not selected -> register them as individual users 
-                    {
-                        await _userManager.AddToRoleAsync(user, SD.Role_User_Indi);
-                    }
+                if (user.Role == null) // role is not selected -> register them as individual users 
+                    await _userManager.AddToRoleAsync(user, SD.Role_User_Indi);
 
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                    //    protocol: Request.Scheme);
 
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    return RedirectToPage("RegisterConfirmation", new {email = Input.Email, returnUrl});
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        if (user.Role == null)  // if user registering from website (not by admin)
-                        {
-                            await _signInManager.SignInAsync(user, isPersistent: false);
-                            return LocalRedirect(returnUrl);
-                             
-                        }
-                        else   // admin registering a new user - don't want to log admin out and sign in new user registered - redirect admin to list of users 
-                        {
-                            return RedirectToAction("Index", "Admin", new { Area = "Admin" });    // Admin Page
-                        }
-                    }
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
+
+                if (user.Role != null) return RedirectToAction("Index", "Admin", new {Area = "Admin"});
+                await _signInManager.SignInAsync(user, false);
+                return LocalRedirect(returnUrl);
             }
 
-            // If we got this far, something failed, redisplay form
+            foreach (var error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
+
+
             return Page();
         }
     }
