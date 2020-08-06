ca0b580a685647977275e6c1bb548dc7b01294b8
Justin Thoms <DyneXvX@outlook.com>
2020-08-06 10:50:18 -0400

    Removed unused using statments Removed non-required commits changed function values to starndards if possbile inverted if's for less nesting issues.

--- VMS/Areas/Admin/Controllers/OpportunityController.cs
+++ VMS/Areas/Admin/Controllers/OpportunityController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VMS.DataAccess.Repository.IRepository;
@@ -20,66 +18,39 @@ namespace VMS.Areas.Admin.Controllers
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        //  Merv & Camille
-        // Upsert -----------------------------------------------------
-
         public IActionResult Upsert(int? id)
         {
-            Opportunity opportunity = new Opportunity();
-            if (id == null)
-            {
-                //this is for create
-                //if empty return to the view an empty category
-                return View(opportunity);
-            }
-
-            /*this is for edit
-            returns default value of the data type of a collection if a 
-            collection is empty or doesn't find any element that satisfies
-            the condition
-            */
+            var opportunity = new Opportunity();
+            if (id == null) return View(opportunity);
 
             opportunity = _unitOfWork.Opportunity.Get(id.GetValueOrDefault());
 
-            //taking care of null/if id is incorrect
-            if (opportunity == null)
-            {
-                return NotFound();
-            }
-            //else
+
+            if (opportunity == null) return NotFound();
             return View(opportunity);
         }
-
 
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public IActionResult Upsert(Opportunity opportunity)
         {
-            if (ModelState.IsValid) //checks all the validations in the model to see if its true. Extra security
-            {
+            if (!ModelState.IsValid) return View(opportunity);
+            if (opportunity.Id == 0)
+                _unitOfWork.Opportunity.Add(opportunity);
+            else
+                _unitOfWork.Opportunity.Update(opportunity);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
 
-                if (opportunity.Id == 0)
-                {
-                    _unitOfWork.Opportunity.Add(opportunity);
-                }
-                else
-                {
-                    _unitOfWork.Opportunity.Update(opportunity);
-                }
-                _unitOfWork.Save();
-                return RedirectToAction(nameof(Index));
-            }
-            // else return back to opportunity page
-            return View(opportunity);
         }
-        //----------------------------------------------------------------------
+
 
         #region API CALLS
 
@@ -87,17 +58,17 @@ namespace VMS.Areas.Admin.Controllers
         public IActionResult GetAll()
         {
             var allObj = _unitOfWork.Opportunity.GetAll();
-            return Json(new { data = allObj });
+            return Json(new {data = allObj});
         }
 
         [HttpGet]
         public IActionResult GetSingle(int id)
         {
             var volObj = _unitOfWork.Opportunity.Get(id);
-            return Json(new { data = volObj });
+            return Json(new {data = volObj});
         }
 
-        // Apply filters to Opportunity table 
+
         [HttpGet]
         public IActionResult GetFilters(string filter)
         {
@@ -106,33 +77,23 @@ namespace VMS.Areas.Admin.Controllers
             if (allObj == null)
             {
                 TempData["Error"] = "Error retrieving Opportunities";
-                return Json(new { success = false, message = "Error while retrieving" });
+                return Json(new {success = false, message = "Error while retrieving"});
             }
 
-            if (filter == null)
-            {
-                return Json(new { data = allObj });    // if no filters, return all
-            }
+            if (filter == null) return Json(new {data = allObj});
 
             if (filter.Equals(SD.Filter_Date))
             {
-                DateTime today = DateTime.Now;
+                var today = DateTime.Now;
                 Func<Opportunity, bool> last60Days = o => o.DatePosted > today.AddDays(-60);
                 allObj = allObj.Where(last60Days);
-                return Json(new { data = allObj });
-            }
-            else if (filter.Equals(""))
-            {
-                return Json(new { data = allObj });    // if no filters, return all 
-            }
-            else  // center type
-            {
-                Func<Opportunity, bool> center = o => o.CenterType == filter;
-                allObj = allObj.Where(center);
-                return Json(new { data = allObj });
+                return Json(new {data = allObj});
             }
 
-            //return Json(new { data = allObj });    // if no filters, return all 
+            if (filter.Equals("")) return Json(new {data = allObj});
+            Func<Opportunity, bool> center = o => o.CenterType == filter;
+            allObj = allObj.Where(center);
+            return Json(new {data = allObj});
         }
 
 
@@ -143,15 +104,14 @@ namespace VMS.Areas.Admin.Controllers
             if (objFromDb == null)
             {
                 TempData["Error"] = "Error deleting Opportunity";
-                return Json(new { success = false, message = "Error while deleting" });
+                return Json(new {success = false, message = "Error while deleting"});
             }
 
             _unitOfWork.Opportunity.Remove(objFromDb);
             _unitOfWork.Save();
 
             TempData["Success"] = "Opportunity successfully deleted";
-            return Json(new { success = true, message = "Delete Successful" });
-
+            return Json(new {success = true, message = "Delete Successful"});
         }
 
         #endregion
